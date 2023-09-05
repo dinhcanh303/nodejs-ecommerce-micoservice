38d935f02f91330448dd8e03a1b1ffd906bfeef0
Ngo Dinh Canh <rapidstars97@gmail.com>
2023-09-05 20:30:05 +0700

    FEAT: finished cart service

--- eCommerce/src/services/cart.service.js
+++ eCommerce/src/services/cart.service.js
@@ -23,15 +23,87 @@ class CartService {
       options = { upsert: true, new: true };
     return await cart.findOneAndUpdate(query, updateOrInsert, options);
   }
+  static async updateUserCartQuantity({ userId, product }) {
+    const { productId, quantity } = product;
+    const query = {
+        cart_user_id: userId,
+        "cart_products.productId": productId,
+        cart_state: "active",
+      },
+      updateSet = {
+        $inc: {
+          "cart_products.$.quantity": quantity,
+        },
+      },
+      options = { upsert: true, new: true };
+    return await cartModel.findOneAndUpdate(query, updateSet, options);
+  }
+
   static async addToCart({ userId, product = {} }) {
-    //check cart exists
-    const userCart = await cart.findOne({ cart_user_id: userId }).lean();
-    if (!userCart) return await this.createUserCart({ userId, product });
-    //if has cart but not product
+    const userCart = await cartModel.findOne({
+      cart_user_id: userId,
+    });
+
+    if (!userCart) {
+      // create cart for User
+      return await CartService.createUserCart({ userId, product });
+    }
+
+    // neu co gio hang roi nhung chua co san pham nao
     if (!userCart.cart_products.length) {
       userCart.cart_products = [product];
       return await userCart.save();
     }
-    
+
+    // gio hang ton tai, va co san pham nay thi update quantity
+    return await CartService.updateUserCartQuantity({ userId, product });
+  }
+
+  static async addToCartV2({ userId, shop_order_ids = [] }) {
+    const { productId, quantity, old_quantity } =
+      shop_order_ids[0]?.item_products[0];
+
+    // check product
+    const foundProduct = await getProductById(productId);
+    if (!foundProduct) throw new Api404Error("Product not found");
+
+    // compare
+    if (foundProduct.product_shop.toString() !== shop_order_ids[0]?.shopId) {
+      throw new Api404Error("Product do not belong to the shop");
+    }
+
+    if (quantity === 0) {
+      // todo deleted
+    }
+
+    return await CartService.updateUserCartQuantity({
+      userId,
+      product: {
+        productId,
+        quantity: quantity - old_quantity,
+      },
+    });
+  }
+
+  static async deleteItemInCart({ userId, productId }) {
+    const query = { cart_user_id: userId, cart_state: "active" };
+    const updateSet = {
+      $pull: {
+        cart_products: {
+          productId,
+        },
+      },
+    };
+
+    return await cartModel.updateOne(query, updateSet);
+  }
+
+  static async getListUserCart({ userId }) {
+    return await cartModel
+      .findOne({
+        cart_user_id: +userId,
+      })
+      .lean();
   }
 }
+module.exports = CartService;
