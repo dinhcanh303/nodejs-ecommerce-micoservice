112440f03191d52ea710b3d4f1052d5199e1b52b
Ngo Dinh Canh <rapidstars97@gmail.com>
2024-05-24 22:58:20 +0700

    insert db mysql

--- eCommerce/src/utils/index.js
+++ eCommerce/src/utils/index.js
@@ -2,6 +2,8 @@
 
 const _ = require("lodash");
 const { Types } = require("mongoose");
+const  crypto = require("crypto");
+
 const getInfoData = ({ fields = [], object = {} }) => {
   return _.pick(object, fields);
 };
@@ -37,10 +39,48 @@ const updateNestedObjectParser = (obj) => {
 };
 const convertObjectIdMongodb = (id) => new Types.ObjectId(id);
 
+const randomUserName = (length = 6) => {
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let username = '';
+  for(let i = 0; i < length; i++) {
+    const idx = Math.floor(Math.random() * characters.length);
+    username += characters[idx];
+  }
+  return username;
+}
+const randomPassword = (length = 6) => {
+  const numbers = '0123456789'
+  let password = '';
+  for(let i = 0; i < length; i++) {
+    const idx = Math.floor(Math.random() * numbers.length);
+    password += numbers[idx];
+  }
+  return password;
+
+}
+const generateUniqueUserNames = (count) => {
+  const usernames = new Set();
+  while(usernames.size < count){
+    let username = randomUserName();
+    usernames.add(username);
+  }
+  return Array.from(usernames);
+}
+const generateUniquePasswords = (count) => {
+  const passwords = new Set();
+  while(passwords.size < count){
+    let password = randomPassword();
+    passwords.add(crypto.createHash("sha256").update(password).digest('hex').toString());
+  }
+  return Array.from(passwords);
+}
+
 module.exports = {
   getInfoData,
   selectData,
   removeUndefinedObject,
   updateNestedObjectParser,
   convertObjectIdMongodb,
+  generateUniqueUserNames,
+  generateUniquePasswords,
 };
